3248c5895092c7cbf79162a32e1b3e771cb3f10d
Naina Prasad <prasad.naina@gmail.com>
2015-10-08 23:30:04 -0400

    working with audio

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ var queryString = ''
 
 var createQuery = function() {
     clearResDiv();
+    offset = 0;
 
     var artist = $('#artist').val();
     var song = $('#song').val();
@@ -42,24 +43,24 @@ var createQuery = function() {
     } else {
         if (artist != '') {
             artist = artist.replace(/ /g, '%20');
-            queryString += "artist:" + artist + "&";
+            queryString += "artist:" + artist + "%20";
         };
         if (song != '') {
             song = song.replace(/ /g, '%20');
-            queryString += "track:" + song + "&";
+            queryString += "track:" + song + "%20";
         };
         if (album != '') {
             album = album.replace(/ /g, '%20');
-            queryString += "album:" + album + "&";
+            queryString += "album:" + album + "%20";
         };
         if (year != '') {
-            queryString += "year:" + year + "&";
+            queryString += "year:" + year + "%20";
         };
         if (genre != '') {
             genre = genre.replace(/ /g, '%20');
-            queryString += "genre:" + genre + "&";
+            queryString += "genre:" + genre + "%20";
         };
-        queryString += "type=track&limit=10"
+        queryString += "&type=track&limit=10"
         lookupQuery(queryString);
         console.log(queryString);
     }
@@ -72,6 +73,7 @@ var lookupQuery = function(queryString) {
     $('html, body').animate({
         scrollTop: $(".allResults").offset().top
     }, 1500);
+
     $.ajax({
         url: queryString,
         type: 'GET',
@@ -113,36 +115,60 @@ var renderSongs = function() {
         var trackHTML = '<tr class="oneSong" data-id="' + songResults[i].tagNumber + '"><td id="coverDiv"><img id="albumCover" src="' + songResults[i].cover + '" alt="heart.png"></img></td><td id="resText">' + songResults[i].name + '</td><td id="resText">' + songResults[i].artist + '</td><td id="resText">' + songResults[i].album + '</td></tr>';
         $(".resultsTable").append(trackHTML);
     }
+
+    var audioObject = 0;
+
     $(".oneSong").bind("click", function() {
         var tempThis = $(this);
-        var tagNum = $(this).attr("data-id")
+        var tagNum = $(this).attr("data-id");
+
         for (var i = 0; i < songResults.length; i++) {
             if (tagNum == songResults[i].tagNumber) {
-                $.ajax({
-                    url: songResults[i].previewURL,
-                    type: 'GET'
-                }).success(function(response) {
-                    alert("GET MP3 SUCCESSFUL")
-                    response.play()
-                    //console.log(response)
-                })
-                //var audioFile = '<audio id="playAudio" src=' + songResults[i].previewURL + 'type="audio/mp3"></audio>'; 
-                //$(tempThis).append(audioFile);
-                //$(tempThis).play();
+                if ($(tempThis).hasClass("playingCssClass")){
+                    audioObject.pause();
+                }
+                else{
+                    if (audioObject) {
+                        audioObject.pause();
+                    }
+                    audioObject = new Audio(songResults[i].previewURL)
+                    audioObject.play();
+                    $(tempThis).addClass("playingCssClass");
+                    audioObject.addEventListener('ended', function () {
+                        $(tempThis).removeClass("playingCssClass");
+                    });
+                    audioObject.addEventListener('pause', function () {
+                        $(tempThis).removeClass("playingCssClass");
+                    });
+                }
+                   
             }
         }
+
+        
     });
-    var resNumLine = '<h3 id="linkToViewMore"> Showing Results ' + ((offset * 10) + 1) + ' - ' + ((offset * 10) + 10) + '    ></h3><br><br>';
-    $(".showResults").append(resNumLine);
+
+
+    var viewNextOrPrevDiv = '<div id="viewNextOrPrevDiv"></div>'
+    $(".showResults").append(viewNextOrPrevDiv)
+
+    if (offset > 0){
+        $("#viewNextOrPrevDiv").addClass("prevExists");
+        var resPrev = '<h3 id="linkToViewLess">< Previous Results </h3>';
+        $("#viewNextOrPrevDiv").append(resPrev);
+        $('#linkToViewLess').bind("click", function() {
+            viewLess();
+        });      
+    }
+    var resNext = '<h3 id="linkToViewMore"> More Results > </h3><br><br>';
+    $("#viewNextOrPrevDiv").append(resNext);
     $('#linkToViewMore').bind("click", function() {
-        // $(".resultsTable").empty();
-        // // $(".allResults").empty();
-        // offset++;
-        // var offsetValue = offset * 10;
-        // queryString += ("&offset=" + offsetValue);
-        // lookupQuery(queryString);
         viewMore();
     });
+
+
+    var currentResDisp = '<h3 id="curRange"> Showing Results: ' + ((offset * 10) + 1) + ' - ' + ((offset * 10) + 10) + '</h3><br>'
+    $(".showResults").append(currentResDisp);
     //var viewMoreButton = '<input type="button" value="View More" id="viewMoreButton" onclick="viewMore();" /><br>';
     //$(".showResults").append(viewMoreButton);
     var searchAgainButton = '<br><input type="button" value="Search Again!" id="secondSearch" onclick="searchAgain();" />';
@@ -162,6 +188,21 @@ var viewMore = function() {
     console.log(queryString);
     lookupQuery(queryString);
 }
+var viewLess = function() {
+    clearResDiv();
+    offset--;
+    var offsetValue = offset * 10;
+    if ((queryString.search("&offset=") + 1)) {
+        queryString = queryString.substring(0, queryString.length - 2);
+        queryString += offsetValue;
+    } else {
+        queryString += ("&offset=" + offsetValue);
+    }
+    console.log(queryString);
+    lookupQuery(queryString);
+}
+
+
 var searchAgain = function() {
     $('html, body').animate({
         scrollTop: $("body").offset().top
